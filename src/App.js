d33adb46684418789121856ffc6cd71b2ada06ec
DArbatskiy <66027751+DArbatskiy@users.noreply.github.com>
2020-07-21 13:32:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { setState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import Info from './components/Info';
@@ -6,49 +6,44 @@ import Stock from './components/Stock';
 
 const API_KEY = 'GR5KIC31BMD0ZSAS';
 
-class App extends React.Component {
+function App(state) {
 
-  state= {
+  state = {
     symbol: undefined,
     lastRefrehed: undefined,
     openPrice: undefined,
     closePrice: undefined
   }
-
-  gettingStock = async (e) => {
+  
+  
+  async function gettingStock(e) {
     e.preventDefault();
     let brand = e.target.elements.brand.value;
     const api_url = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${brand}&apikey=${API_KEY}`);
     const data = await api_url.json();
-    console.log(data);
-    console.log(data["Meta Data"]["2. Symbol"]);
 
     let lastRefrehed = data["Meta Data"]["3. Last Refreshed"];
-    let symbol = "2. Symbol"
-    console.log(data["Meta Data"][`${symbol}`]);
 
-   this.setState({
-      symbol: data["Meta Data"]["2. Symbol"],
-      lastRefrehed: data["Meta Data"]["3. Last Refreshed"],
-      openPrice: data["Time Series (Daily)"][`${lastRefrehed}`]["1. open"],
-      closePrice: data["Time Series (Daily)"][`${lastRefrehed}`]["4. close"]
-    });
+    setState({
+        symbol: data["Meta Data"]["2. Symbol"],
+        lastRefrehed: data["Meta Data"]["3. Last Refreshed"],
+        openPrice: data["Time Series (Daily)"][`${lastRefrehed}`]["1. open"],
+        closePrice: data["Time Series (Daily)"][`${lastRefrehed}`]["4. close"]
+      });
   }
 
-  render() {
-    return (
-      <div>
-        <Info />
-        <Form stockMethod={this.gettingStock} />
-        <Stock 
-          symbol = {this.state.symbol}
-          lastRefrehed = {this.state.lastRefrehed}
-          openPrice = {this.state.openPrice}
-          closePrice = {this.state.closePrice}
-        />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Info />
+      <Form stockMethod={gettingStock} />
+      <Stock 
+        symbol = {state.symbol}
+        lastRefrehed = {state.lastRefrehed}
+        openPrice = {state.openPrice}
+        closePrice = {state.closePrice}
+      />
+    </div>
+  );
 }
 
 export default App;
