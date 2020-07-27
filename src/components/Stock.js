b098b4231c4c8a6ae11eb474a6ce3a0b3df00132
DArbatskiy <66027751+DArbatskiy@users.noreply.github.com>
2020-07-27 22:41:36 +0300

    Update Stock.js

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
-class Stock extends React.Component {
-  render() {
-    return (
-      <div>
-        <p>Symbol: {this.props.symbol}</p>
-        <p>LastRefrehed: {this.props.lastRefrehed}</p>
-        <p>Open price: {this.props.openPrice}</p>
-        <p>Close price: {this.props.closePrice}</p>
-      </div>
-    );
-  }
+function Stock(props) {
+  const {symbol, lastRefreshed, openPrice, closePrice} = props;
+  return (
+    <div>
+      <p>Symbol: {symbol}</p>
+      <p>LastRefreshed: {lastRefreshed}</p>
+      <p>Open price: {openPrice}</p>
+      <p>Close price: {closePrice}</p>
+    </div>
+  );
 }
 
 export default Stock;
