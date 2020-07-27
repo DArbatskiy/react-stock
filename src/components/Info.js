3db6c6ed2a4612669efd05953f3afdf97a3fd351
DArbatskiy <66027751+DArbatskiy@users.noreply.github.com>
2020-07-27 22:41:14 +0300

    Update Info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-class Info extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2>Stocks</h2>
-        <p>Info about stocks</p>
-      </div>
-    );
-  }
+function Info() {
+  return (
+    <div>
+      <h2>Stocks</h2>
+      <p>Info about stocks</p>
+    </div>
+  );
 }
 
 export default Info;
