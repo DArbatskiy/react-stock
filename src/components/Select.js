19968432794d60a89f5584edb018d873b53ae200
DArbatskiy <66027751+DArbatskiy@users.noreply.github.com>
2020-08-05 18:47:17 +0300

    Update Select.js

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,15 +1,42 @@
 import React, {useState} from 'react';
+import Stock from './Stock';
 
 export default function Select(props) {
   const{companies} = props;
   const [brand, setBrand] = useState('');
-  for (let company in companies) {
-    let brand = companies[company].brand;
+
+  const [ticker, setTicker] = useState('');
+  const [lastRefreshed, setLastRefreshed] = useState('');
+  const [openPrice,setOpenPrice] = useState('');
+  const [closePrice, setClosePrice] = useState('');
+
+  async function handleButtonClick() {
+    const API_KEY = 'GR5KIC31BMD0ZSAS';
+    const api_url = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${brand}&apikey=${API_KEY}`);
+    const data = await api_url.json();
+
+    let lastRefreshed = data["Meta Data"]["3. Last Refreshed"];
+
+    setTicker(data["Meta Data"]["2. Symbol"]);
+    setLastRefreshed(data["Meta Data"]["3. Last Refreshed"]);
+    setOpenPrice(data["Time Series (Daily)"][`${lastRefreshed}`]["1. open"]);
+    setClosePrice(data["Time Series (Daily)"][`${lastRefreshed}`]["4. close"]);
+
   }
+  
 
   return (
-    <select value={brand} onChange={e=>setBrand(e.target.value)}>
-      {props.brands.map((brand,index) => <option value={brand}>{brand}</option>)}
-    </select>
+    <>
+      <select value={brand} onChange={e=>setBrand(e.target.value)}>
+        {companies.map(company => <option value={company.symbol}>{company.brand}</option>)}
+      </select>
+      <button onClick={handleButtonClick}>Get Info</button>
+      {ticker &&< Stock 
+          ticker = {ticker}
+          lastRefreshed = {lastRefreshed}
+          openPrice = {openPrice}
+          closePrice = {closePrice}
+      />}
+    </>
   );
 } 
