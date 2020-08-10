fa71b7fda0c3052636ad5a7424c14d16f8a3dcc5
DArbatskiy <66027751+DArbatskiy@users.noreply.github.com>
2020-08-10 23:48:43 +0300

    Update Stock.js

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -4,6 +4,9 @@ function Stock(props) {
   const {ticker, lastRefreshed, openPrice, closePrice} = props;
   return (
     <div>
+      <img src={process.env.PUBLIC_URL + `/logos/${ticker}.svg`} 
+        alt='logo' 
+        style={{height: 100, width: 100, borderRadius: '50%', backgroundColor: 'white'}}/>
       <p>Ticker: {ticker}</p>
       <p>LastRefreshed: {lastRefreshed}</p>
       <p>Open price: {openPrice}</p>
